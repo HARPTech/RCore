18d5c72233a758668d4522ceb0164203d19abbda
Max Heisinger <mail@maximaximal.com>
2018-09-28 18:05:49 +0200

    Add ostream for RCore events.

--- librcore/include/RCore/events.h
+++ librcore/include/RCore/events.h
@@ -8,25 +8,44 @@ extern "C"
 
   typedef enum lrt_rcore_event_t
   {
-    LRT_RCORE_OK = 0,
-    LRT_RCORE_INVALID_BLOCK = 1,
-    LRT_RCORE_STACK_FULL = 2,
-    LRT_RCORE_ACK_STACK_MAP_FULL = 3,
-    LRT_RCORE_ACK_STACK_QUEUE_FULL = 4,
-    LRT_RCORE_STACK_DEPTH_EXHAUSTED = 5,
-    LRT_RCORE_NO_ACK_ENTRY_FOUND = 6,
-    LRT_RCORE_MAXIMUM_SUBSCRIPTIONS = 7,
-    LRT_RCORE_SUBSCRIPTION_MAP_PUT_ERROR = 8,
-    LRT_RCORE_TRANSMIT_ERROR = 9,
-    LRT_RCORE_DATA_LEFT = 10,
-    LRT_RCORE_ALLOC_FAILED = 11,
-    LRT_RCORE_INTERNAL_SEQUENCE_STACK_ERROR = 12,
-    LRT_RCORE_GENERIC_ACCEPTOR_ERROR = 13,
-    LRT_RCORE_GENERIC_TRANSMITTER_ERROR = 14
+    LRT_RCORE_OK,
+    LRT_RCORE_INVALID_BLOCK,
+    LRT_RCORE_STACK_FULL,
+    LRT_RCORE_ACK_STACK_MAP_FULL,
+    LRT_RCORE_ACK_STACK_QUEUE_FULL,
+    LRT_RCORE_NO_ACK_ENTRY_FOUND,
+    LRT_RCORE_STACK_DEPTH_EXHAUSTED,
+    LRT_RCORE_MAXIMUM_SUBSCRIPTIONS,
+    LRT_RCORE_SUBSCRIPTION_MAP_PUT_ERROR,
+    LRT_RCORE_TRANSMIT_ERROR,
+    LRT_RCORE_DATA_LEFT,
+    LRT_RCORE_ALLOC_FAILED,
+    LRT_RCORE_INTERNAL_SEQUENCE_STACK_ERROR,
+    LRT_RCORE_GENERIC_ACCEPTOR_ERROR,
+    LRT_RCORE_GENERIC_TRANSMITTER_ERROR,
+    LRT_RCORE_NOT_ACCEPTED,
+    LRT_RCORE_NOT_SUBSCRIBED,
+    LRT_RCORE_INVALID_TRANSMIT_BUFFER_ENTRY,
+
+    LRT_RCORE_EVENT_COUNT
   } lrt_rcore_event_t;
+
+  extern const char* lrt_rcore_event_names[LRT_RCORE_EVENT_COUNT];
 
 #ifdef __cplusplus
 }// closing brace for extern "C"
+#endif
+
+#if defined(__cplusplus) && !defined(DISABLE_ADVANCED_CPP)
+// Automatic conversion into string for enum when using C++.
+#include <iostream>
+inline std::ostream&
+operator<<(std::ostream& os, const lrt_rcore_event_t& e)
+{
+  os << "RCore Event " << static_cast<size_t>(e) << ": "
+     << lrt_rcore_event_names[static_cast<size_t>(e)];
+  return os;
+}
 #endif
 
 #endif
---
+++ librcore/src/events.c
@@ -0,0 +1,22 @@
+#include "../include/RCore/events.h"
+
+const char* lrt_rcore_event_names[LRT_RCORE_EVENT_COUNT] = {
+  "Ok",
+  "Invalid Block",
+  "Stack Full",
+  "[ACK Stack] Full map",
+  "[ACK Stack] Full queue",
+  "[ACK Stack] No entry found",
+  "Stack depth exhausted",
+  "[Subscription Map] Maximum subscriptions reached",
+  "[Subscription Map] Put error",
+  "Transmit error",
+  "Data left",
+  "Alloc failed",
+  "[Sequence Stack] Internal error",
+  "[Generic] Acceptor error",
+  "[Generic] Transmitter error",
+  "Not accepted",
+  "Not subscribed",
+  "[Transmit Buffer] Invalid entry"
+};
